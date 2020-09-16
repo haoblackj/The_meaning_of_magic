f26232f79ec2765f7ec2dac40d8634fc7d71d6b2
haoblackj <yuufuture.springfieldxd9@gmail.com>
2020-09-16 22:28:31 +0900

    202009162228不要ファイル除外

--- pdf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for %%i in ( %* ) do (
-    "N:\izuna\Izuna_cmd.exe" -s "N:\tool\MF\MF_hiro.conf" -p ZZ_20191006175415_YpQhRqUgDj -f %%i -q
-)
--- pdf_作業用.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-for %%i in ( sts*.txt ) do (
-    "N:\izuna\Izuna_cmd.exe" -s "N:\tool\MF\MF_hiro.conf" -p ZZ_20191006175415_YpQhRqUgDj -f %%i -q
-)
